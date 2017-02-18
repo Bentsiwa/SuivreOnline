d24110751af0eb6782dde7c81746a33808f49975
Bentsiwa <efuabainson@gmail.com>
2017-02-18 00:45:41 +0000

    adding image to database

--- js/index.js
+++ js/index.js
@@ -65,6 +65,7 @@
 		smallImage.style.display = 'block';
 
 		smallImage.src = "data:image/jpeg;base64," + imageData;
+		movePic(imageData);
 
 	}
 
@@ -106,5 +107,39 @@
 	 function onBarcodeFail(error) {
 	          alert("Scanning failed: " + error);
 	      	}
+
+
+	 function movePic(file){ 
+    	window.resolveLocalFileSystemURI(file, resolveOnSuccess, resOnError); 
+	} 
+
+	//Callback function when the file system uri has been resolved
+	function resolveOnSuccess(entry){ 
+	    var d = new Date();
+	    var n = d.getTime();
+	    //new file name
+	    var newFileName = n + ".jpg";
+	    var myFolderApp = "Suivre";
+
+	    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys) {      
+	    //The folder is created if doesn't exist
+	    fileSys.root.getDirectory( myFolderApp,
+	                    {create:true, exclusive: false},
+	                    function(directory) {
+	                        entry.moveTo(directory, newFileName,  successMove, resOnError);
+	                    },
+	                    resOnError);
+	                    },
+	    resOnError);
+	}
+
+//Callback function when the file has been moved successfully - inserting the complete path
+function successMove(entry) {
+    //I do my insert with "entry.fullPath" as for the path
+}
+
+function resOnError(error) {
+    alert(error.code);
+}
 
 })();
